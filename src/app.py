7af9d173567718e12ad4de7187c3ee5b4cbe8d87
Ken Wu <wooqianghao@gmail.com>
2024-06-16 09:15:02 -0400

    Initial experiment

---
+++ src/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from chatbot import ChatBot, SYSTEM_MESSAGE_PROMPT
+from langchain_core.messages import AIMessage, HumanMessage
+
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+# uploaded_files = st.sidebar.file_uploader("Upload image", type=['png', 'jpg', 'pdf'], accept_multiple_files=True)
+
+st.title("MedChat Demo")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message.type):
+        st.markdown(message.content)
+
+st.session_state.bot = ChatBot()
+
+if prompt := st.chat_input("Text here..."):
+    with st.chat_message("User"):
+        st.markdown(prompt)
+
+    with st.chat_message("AI"):
+        message_placeholder = st.empty()
+        full_response = st.session_state.bot.query(
+            prompt,
+            chat_history=st.session_state.messages
+        )
+        message_placeholder.markdown(full_response)
+
+    st.session_state.messages.append(AIMessage(content=full_response))
